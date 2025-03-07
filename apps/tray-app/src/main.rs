181cc3b6718bb5c048b81fa68a3deca03db10940
sinanovicanes <sinanovicanes01@gmail.com>
2025-03-07 16:03:14 +0300

    feat: Implement Bluetooth LE advertisement scanning functionality

--- apps/tray-app/src/main.rs
+++ apps/tray-app/src/main.rs
@@ -1,9 +1,46 @@
-use device::managers::DeviceManager;
+use tokio::signal;
+use windows::{
+    core::{Error, Ref},
+    Devices::Bluetooth::Advertisement::{
+        BluetoothLEAdvertisementReceivedEventArgs, BluetoothLEAdvertisementWatcher,
+        BluetoothLEScanningMode,
+    },
+    Foundation::TypedEventHandler,
+};
 
 #[tokio::main]
-async fn main() {
-    let mut device_manager = DeviceManager::new();
-    let _ = device_manager.scan();
+async fn main() -> windows::core::Result<()> {
+    let watcher = BluetoothLEAdvertisementWatcher::new()?;
+    watcher.SetScanningMode(BluetoothLEScanningMode::Active)?;
 
-    println!("{:?}", device_manager);
+    watcher.Received(&TypedEventHandler::<
+        BluetoothLEAdvertisementWatcher,
+        BluetoothLEAdvertisementReceivedEventArgs,
+    >::new(|_watcher, args| {
+        println!("Received advertisement.");
+        let device = args.as_ref().ok_or(Error::empty())?;
+        let address = device.BluetoothAddress()?;
+        let manufacturer_data = device.Advertisement()?.ManufacturerData()?;
+
+        for data in manufacturer_data {
+            let company_id = data.CompanyId()?;
+            let data = data.Data()?;
+
+            println!("Company ID: {}", company_id);
+            println!("Data: {:?}", data);
+        }
+
+        println!("Address: {}", address);
+        Ok(())
+    }))?;
+
+    watcher.Start()?;
+    println!("Scanning for Bluetooth LE advertisements...");
+
+    signal::ctrl_c().await.expect("Failed to listen for Ctrl+C");
+
+    watcher.Stop()?;
+
+    println!("Stopped scanning.");
+    Ok(())
 }
