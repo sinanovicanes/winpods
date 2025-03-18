02937d02223de43ebad23638969899d83d67a5fd
sinanovicanes <sinanovicanes01@gmail.com>
2025-03-18 18:30:05 +0300

    refactor(releases-api): Introduce ReleaseService class

--- apps/releases-api/src/index.ts
+++ apps/releases-api/src/index.ts
@@ -1,68 +1,5 @@
-import { GithubAPI } from './github_api';
+import { ReleaseService } from './release.service';
 import { isValidVersionString } from './utils';
-
-interface ReleasePlatform {
-	signature: string;
-	url: string;
-}
-
-interface ReleaseInfo {
-	version: string;
-	notes: string;
-	pub_date: string;
-	platforms: {
-		['windows-x86_64']: ReleasePlatform;
-	};
-}
-
-async function createLatestReleaseInfo(repo: string): Promise<ReleaseInfo> {
-	const latestRelease = await GithubAPI.getLatestRelease(repo);
-	const url = latestRelease.assets.find((asset) => asset.name.endsWith('x64_en-US.msi.zip'))?.browser_download_url;
-	const signatureUrl = latestRelease.assets.find((asset) => asset.name.endsWith('x64_en-US.msi.zip.sig'))?.browser_download_url;
-
-	if (!url || !signatureUrl) {
-		throw new Error('Failed to find download url or signature url');
-	}
-
-	const signature = await fetch(signatureUrl).then((res) => res.text());
-
-	if (!signature) {
-		throw new Error('Failed to fetch signature');
-	}
-
-	return {
-		version: latestRelease.tag_name,
-		notes: latestRelease.body,
-		pub_date: latestRelease.published_at,
-		platforms: {
-			'windows-x86_64': {
-				signature,
-				url,
-			},
-		},
-	};
-}
-
-async function getLatestReleaseInfo(env: Env): Promise<ReleaseInfo> {
-	const CACHE_KEY = env.CACHE_KEY || 'winpods:latest-release';
-	const CACHE_TTL = env.CACHE_TTL || 300;
-	const kv = env.KV;
-
-	const cachedRelease = await kv.get(CACHE_KEY);
-
-	if (cachedRelease) {
-		console.log('Returning cached release');
-		return JSON.parse(cachedRelease);
-	}
-
-	const release = await createLatestReleaseInfo(env.GITHUB_REPO);
-	console.log('Caching release');
-	await kv.put(CACHE_KEY, JSON.stringify(release), {
-		expirationTtl: CACHE_TTL,
-	});
-
-	return release;
-}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
@@ -79,7 +16,8 @@ export default {
 		}
 
 		try {
-			const release = await getLatestReleaseInfo(env);
+			const releaseService = new ReleaseService(env);
+			const release = await releaseService.getLatestReleaseInfo();
 
 			if (release.version.replace('v', '') === version) {
 				console.log('No updates available for version: ' + version);
