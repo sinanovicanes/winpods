ea5277fed9e66e9a1601cf4c41dbae126bd14ea5
sinanovicanes <sinanovicanes01@gmail.com>
2025-03-24 20:18:47 +0300

    feat(github-api): Rename interfaces for clarity and add getReleaseByTag method

--- packages/github-api/index.ts
+++ packages/github-api/index.ts
@@ -1,17 +1,17 @@
-export interface GithubAsset {
+export interface AssetInfo {
   name: string;
   browser_download_url: string;
 }
 
-export interface GithubResponse {
+export interface ReleaseInfo {
   tag_name: string;
   body: string;
   published_at: string;
-  assets: GithubAsset[];
+  assets: AssetInfo[];
 }
 
 export class GithubAPI {
-  static async getLatestRelease(repo: string): Promise<GithubResponse> {
+  static async getLatestRelease(repo: string): Promise<ReleaseInfo> {
     const response = await fetch(`https://api.github.com/repos/${repo}/releases/latest`, {
       method: "GET",
       headers: {
@@ -23,6 +23,27 @@ export class GithubAPI {
     if (!response.ok) {
       throw new Error(
         `[GITHUB_API]: Failed to fetch latest release: ${repo} ${response.status} ${response.statusText}`
+      );
+    }
+
+    return await response.json();
+  }
+
+  static async getReleaseByTag(repo: string, tag: string): Promise<ReleaseInfo> {
+    const response = await fetch(
+      `https://api.github.com/repos/${repo}/releases/tags/${tag}`,
+      {
+        method: "GET",
+        headers: {
+          "User-Agent": "request",
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `[GITHUB_API]: Failed to fetch release with tag: ${repo} ${tag} ${response.status} ${response.statusText}`
       );
     }
 
