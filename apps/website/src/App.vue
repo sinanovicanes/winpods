b54f5e25bb3440a986873718dc98e79bab13405c
sinanovicanes <sinanovicanes01@gmail.com>
2025-03-25 19:33:09 +0300

    feat(website): Add dynamic social links to header and simplify WPNav component

--- apps/website/src/App.vue
+++ apps/website/src/App.vue
@@ -1,11 +1,38 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import WPNav from './components/WPNav.vue';
+import GithubIcon from '@/assets/github-mark-white.svg';
+
+interface Social {
+  name: string;
+  url: string;
+  icon: string;
+}
+
+const socials: Social[] = [
+  {
+    name: 'Github',
+    url: import.meta.env.VITE_GITHUB_URL,
+    icon: GithubIcon,
+  },
+];
 </script>
 
 <template>
-  <header>
+  <header class="w-full flex items-center justify-between py-4 px-50">
+    <img src="/favicon.ico" alt="winpods" width="50px" />
     <WPNav />
+    <div class="flex gap-1 items-center">
+      <a
+        v-for="social in socials"
+        :key="social.name"
+        :href="social.url"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img :src="social.icon" :alt="social.name" width="24px" />
+      </a>
+    </div>
   </header>
   <RouterView />
 </template>
--- apps/website/src/components/WPNav.vue
+++ apps/website/src/components/WPNav.vue
@@ -1,31 +1,14 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 
-interface Props {
-  withIcon?: boolean;
-}
-
-withDefaults(defineProps<Props>(), {
-  withIcon: true,
-});
-
-const githubURL = import.meta.env.VITE_GITHUB_URL;
 const routes = useRouter().getRoutes();
 </script>
 
 <template>
-  <nav class="w-full text-md px-50 flex justify-between items-center">
-    <img v-if="withIcon" src="/favicon.ico" alt="winpods" width="50px" />
-    <div class="flex items-center justify-center">
-      <router-link v-for="route in routes" :key="route.path" :to="route.path">
-        {{ route.meta.title }}
-      </router-link>
-    </div>
-    <div>
-      <a :href="githubURL" target="_blank" rel="noopener noreferrer">
-        <img src="@/assets/github-mark-white.svg" alt="winpods" width="24px" />
-      </a>
-    </div>
+  <nav class="w-full text-md flex items-center justify-center">
+    <router-link v-for="route in routes" :key="route.path" :to="route.path">
+      {{ route.meta.title }}
+    </router-link>
   </nav>
 </template>
 
