55f8085147515fa6dbd75b0538f99780fba97725
sinanovicanes <sinanovicanes01@gmail.com>
2025-04-01 01:07:49 +0300

    refactor(website): Refactor FAQ section to use dynamic data binding for questions and answers

--- apps/website/src/views/FAQView.vue
+++ apps/website/src/views/FAQView.vue
@@ -1,25 +1,34 @@
+<script setup lang="ts">
+const faq = [
+  {
+    question: 'How do I create an account?',
+    answer:
+      'Click the "Sign Up" button in the top right corner and follow the registration process.',
+  },
+  {
+    question: 'I forgot my password. What should I do?',
+    answer:
+      'Click on "Forgot Password" on the login page and follow the instructions sent to your email.',
+  },
+  {
+    question: 'How do I update my profile information?',
+    answer: 'Go to "My Account" settings and select "Edit Profile" to make changes.',
+  },
+];
+</script>
+
 <template>
   <main class="container grid grid-cols-1 gap-4 pt-5">
     <h1 class="text-3xl font-semibold">Frequently Asked Questions</h1>
-    <div class="collapse collapse-arrow bg-base-100 border border-base-300">
-      <input type="radio" name="faq" :checked="true" />
-      <div class="collapse-title font-semibold">How do I create an account?</div>
+    <div
+      v-for="(q, i) in faq"
+      :key="i"
+      class="collapse collapse-arrow bg-base-100 border border-base-300"
+    >
+      <input type="radio" name="faq" :checked="i === 0" />
+      <div class="collapse-title font-semibold text-lg">{{ q.question }}</div>
       <div class="collapse-content text-sm">
-        Click the "Sign Up" button in the top right corner and follow the registration process.
-      </div>
-    </div>
-    <div class="collapse collapse-arrow bg-base-100 border border-base-300">
-      <input type="radio" name="faq" />
-      <div class="collapse-title font-semibold">I forgot my password. What should I do?</div>
-      <div class="collapse-content text-sm">
-        Click on "Forgot Password" on the login page and follow the instructions sent to your email.
-      </div>
-    </div>
-    <div class="collapse collapse-arrow bg-base-100 border border-base-300">
-      <input type="radio" name="faq" />
-      <div class="collapse-title font-semibold">How do I update my profile information?</div>
-      <div class="collapse-content text-sm">
-        Go to "My Account" settings and select "Edit Profile" to make changes.
+        {{ q.answer }}
       </div>
     </div>
   </main>
