6ce709bd3c5696177a0eb339f302b12548912aca
sinanovicanes <sinanovicanes01@gmail.com>
2025-04-09 10:53:02 +0300

    chore: update dependencies

--- crates/desktop/src/main.ts
+++ crates/desktop/src/main.ts
@@ -2,7 +2,7 @@ import "./index.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
-import { createPlugin } from "tauri-plugin-pinia";
+import { createPlugin } from "@tauri-store/pinia";
 
 const app = createApp(App);
 const pinia = createPinia().use(createPlugin());
